9a40666bb62f41048244c29c67118627665ffea2
HumpelHen <hwellmann@gmx.de>
2025-11-22 21:14:40 +0100

    Delete api directory

--- api/bubble.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  const bubbleIndex = Math.round((Math.random()*0.4 + Math.random()*0.4 + Math.random()*0.2) * 100);
-  res.status(200).json({ bubbleIndex });
-}
--- api/crash.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  const crashProbability = Math.round((Math.random()*0.5 + Math.random()*0.3 + Math.random()*0.2) * 100);
-  res.status(200).json({ crashProbability });
-}
