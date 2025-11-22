2265290d5c0838c69d9b59a4c8f65a3eb8419ba5
HumpelHen <hwellmann@gmx.de>
2025-11-22 21:46:30 +0100

    Delete api directory

--- api/bubble.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  runtime: "nodejs20.x"
-};
-
-export default function handler(req, res) {
-  const idx = Math.round((Math.random()*0.4 + Math.random()*0.4 + Math.random()*0.2) * 100);
-  res.status(200).json({ bubbleIndex: idx });
-}
--- api/crash.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  runtime: "nodejs20.x"
-};
-
-export default function handler(req, res) {
-  const crash = Math.round((Math.random()*0.5 + Math.random()*0.3 + Math.random()*0.2) * 100);
-  res.status(200).json({ crashProbability: crash });
-}
